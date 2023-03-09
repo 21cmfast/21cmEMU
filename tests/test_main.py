03d454ee22ba7999709d75172ad15f3387937449
Steven Murray <steven.murray@curtin.edu.au>
2023-03-09 03:56:52 -0700

    fix: import for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 """Test cases for the __main__ module."""
 import numpy as np
 
-from py21cmemu import py21cmEMU
+from py21cmemu import Emulator
 
 
 def test_basic_prediction():
     """Simply test that we can make a prediction without erroring."""
-    emu = EMU21cmFAST(version="latest")
+    emu = Emulator(version="latest")
     theta = np.random.rand(9 * 5).reshape((5, 9))
     emu.predict(theta)
