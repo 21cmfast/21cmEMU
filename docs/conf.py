be2969a5bfda34690849a0aca19797073f52b811
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-05-18 11:31:38 +0200

    Added setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 import sys
 from pathlib import Path
 import subprocess
+import py21cmemu
 
 sys.path.insert(0, str(Path(__file__).absolute().parent.parent / "src"))
 
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""Setup the package."""
+
+from setuptools import setup
+
+
+setup(
+    name="py21cmemu",
+    version="1.0.0",
+    description="An emulator of 21cmFAST summary statistics.",
+    long_description="",
+    author="Daniela Breitman",
+    author_email="daniela.breitman@sns.it",
+    license="MIT",
+    packages=["py21cmemu"],
+    zip_safe=False,
+)
